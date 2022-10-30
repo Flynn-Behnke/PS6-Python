859f12fd013d152d8a15797c3ce3913b378ad4b4
Flynn-Behnke <113999225+Flynn-Behnke@users.noreply.github.com>
2022-10-30 18:02:43 -0500

    Update PS6P2.py

--- PS6P2.py
+++ PS6P2.py
@@ -1,11 +1,9 @@
-count = 1
-num1 = 1
+num1 = 0
 num2 = 1
 print(num1)
 print(num2)
-while count <= 18:
+for x in range (1,20,1):
   new = num1+num2
   print(new)
   num1 = num2
   num2 = new
-  count = count+1
